4afa6aceb7c939b3741d12ca29235a686f99441d
zhpmatrix <zhpmatrix@gmail.com>
2017-01-11 00:54:46 +0800

    update preprocessor

--- preProcess.py
+++ preProcess.py
@@ -1,5 +1,34 @@
 import pandas as pd
 
+def mergeFile(filePath1,filePath2,shop_id):
+	'''
+		desc:merge user_view and user_pay for specific user
+		param:
+			filePath1:path of user_pay
+			filePath2:path of user_view
+	'''
+
+	# load user data(including user_pay and user_view)
+	user_pay = pd.read_csv(filePath1,header=None,names=['shop_id','time_stamp','pay_sum'])
+	user_view = pd.read_csv(filePath2,header=None,names=['shop_id','time_stamp','view_sum'])
+	
+	# choose cols in user data to merge
+	pay = user_pay.loc[:,['time_stamp','pay_sum']]
+	view = user_view.loc[:,['time_stamp','view_sum']]
+	merge = pd.merge(view,pay,how='outer')
+	headerShop = ['city_name','location_id','per_pay','score','comment_cnt','shop_level','cate_1_name','cate_2_name','cate_3_name']
+	
+	# load shop info
+	shop_info = pd.read_csv('shop_info.txt')
+	
+	# merge cols in shop info
+	for header in headerShop:
+		merge[header]= list(shop_info[shop_info['shop_id']==shop_id][header].iteritems())[0][1]
+	f = open('merge/'+str(shop_id),'w')
+	merge.to_csv(f,index=False)
+	f.close()
+
+
 def splitFile(filePath,fileName,shopNum=2000):
 	groupData = pd.read_csv(filePath,header=None,names=['shop_id','time_stamp','sum'])
 	for i in range(1,shopNum+1):
@@ -26,6 +55,13 @@ if __name__ == "__main__":
 			'user_pay.txt':'user_pay_group.txt'}
 	#for key in dataFiles:
 	#	groupFile(key,dataFiles[key])
-	splitDataFiles = {'user_view_group.txt':'user_view'}
-	for key in splitDataFiles:
-		splitFile(key,splitDataFiles[key])
+	splitDataFiles = {'user_pay_group.txt':'user_pay'}
+	#for key in splitDataFiles:
+	#	splitFile(key,splitDataFiles[key])
+	
+	#merge file
+	numShop = 2000
+	for i in range(1,numShop+1):
+		user_view = 'user_view/user_view-'+str(i)
+		user_pay= 'user_pay/user_pay-'+str(i)
+		mergeFile(user_pay,user_view,i)
