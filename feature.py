18816d30eeb93a9d471cfcb4229d116d1e1355df
zhpmatrix <zhpmatrix@gmail.com>
2017-01-14 20:19:04 +0800

    version1.0

---
+++ feature.py
@@ -0,0 +1,113 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+def _min(sequence):
+	'''
+		desc: find min
+		param: 
+			sequence: list
+	'''
+	if len(sequence) < 1:
+		return None
+	else:
+		return min(sequence)
+def avg(sequence):
+	'''
+		desc: compute average
+		param:
+			sequence: list
+	'''
+	if len(sequence) < 1:
+		return None
+	else:
+		return sum(sequence) / len(sequence)  
+def median(sequence):
+	'''
+		desc: find the median value of the list
+		param:
+			sequence: list
+	'''
+	if len(sequence) < 1:
+		return None
+	else:
+		sequence.sort()
+		return sequence[len(sequence) // 2]
+def m2M(data,x):
+	'''
+		desc: compute the middle value of the last two months for x
+		param:
+			data: sorted by time_stamp
+			x: datetime
+	'''
+	pays = []
+	loc = list(data['time_stamp']).index(pd.Timestamp(x))
+	monthDays = 30
+	for i in range(1,monthDays*2):
+		print data.loc[loc-i]['time_stamp'],data.loc[loc-i]['pay_sum']
+		pays.append(data.loc[loc-i]['pay_sum'])
+	return median(pays)
+def w2A(data,x):
+	'''
+		desc: compute average value of the last two weeks
+		param:
+			data: sorted by time_stamp
+			x: datetime
+	'''
+	pays = []
+	loc = list(data['time_stamp']).index(pd.Timestamp(x))
+	weekDays = 7
+	for i in range(1,weekDays*2):
+		print data.loc[loc-i]['time_stamp'],data.loc[loc-i]['pay_sum']
+		pays.append(data.loc[loc-i]['pay_sum'])
+	return avg(pays)
+def m1L(data,x):
+	'''
+		desc: compute the least valus of the last month
+		param: 
+			x: datetime
+	'''
+	pays = []
+	loc = list(data['time_stamp']).index(pd.Timestamp(x))
+	monthDays = 30
+	for i in range(1,monthDays*1):
+		print data.loc[loc-i]['time_stamp'],data.loc[loc-i]['pay_sum']
+		pays.append(data.loc[loc-i]['pay_sum'])
+	return _min(pays)
+
+def d3w3(data,x):
+	'''
+		desc: compute the average value of the last 3 days and divided by the last 3 weeks
+		param:
+			x: datetime
+	'''
+	pays3D = []
+	pays3W = []
+	loc = list(data['time_stamp']).index(pd.Timestamp(x))
+	weekDays = 30
+	for i in range(1,weekDays*3):
+		print data.loc[loc-i]['time_stamp'],data.loc[loc-i]['pay_sum']
+		if i<= 3:
+			pays3D.append(data.loc[loc-i]['pay_sum'])
+			pays3W.append(data.loc[loc-i]['pay_sum'])
+		else:
+			pays3W.append(data.loc[loc-i]['pay_sum'])
+	return float(avg(pays3D))/avg(pays3W)
+
+if __name__ == '__main__':
+	data = pd.read_csv('raw-data/1',index_col='time_stamp')
+	sortedData = data.sort_index()
+	index = np.array([x for x in range(len(sortedData))])
+	sortedData=sortedData.reset_index()
+	sortedData['time_stamp'] = pd.to_datetime(sortedData['time_stamp'])
+	realStartTime = '2016-09-01'
+	
+	m2Data = sortedData[sortedData['time_stamp'] >= pd.Timestamp(realStartTime)]
+	m2Data['dayofweek'] = map(lambda x:x.dayofweek,m2Data['time_stamp'])
+	m2Data['m2M'] = map(lambda x:m2M(sortedData,x),m2Data['time_stamp'])
+	m2Data['w2A'] = map(lambda x:w2A(sortedData,x),m2Data['time_stamp'])
+	m2Data['m1L'] = map(lambda x:m1L(sortedData,x),m2Data['time_stamp'])
+	m2Data['d3w3'] = map(lambda x:d3w3(sortedData,x),m2Data['time_stamp'])
+    	f = open('1','w')
+	niceData = m2Data.loc[:,['dayofweek','m2M','w2A','m1L','d3w3','pay_sum']]
+	niceData.to_csv(f,index=False)
+	f.close()
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from feature import *
 import numpy as np
 import xgboost as xgb
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.cross_validation import KFold
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
@@ -12,16 +15,13 @@ rng = np.random.RandomState(10)
 # model weights
 weights = [0.20,0.30,0.40,0.10]
 
-# predict vals
-preVals = []
-
-# combination vals with weights
-comVals = []
-
 # load data(506x13)
-boston = load_boston()
-y = boston['target']
-X = boston['data']
+#boston = load_boston()
+#y = boston['target']
+#X = boston['data']
+data = pd.read_csv('1')
+y = data.loc[:,['pay_sum']].as_matrix()
+X = data.loc[:,['dayofweek','m2M','w2A','m1L','d3w3']].as_matrix()
 
 # models set
 regressors = []
@@ -46,17 +46,91 @@ regressors.append(rfr)
 for reg in regressors:
 	kf = KFold(y.shape[0], n_folds=3, shuffle=True, random_state=rng)
 	for train_index, test_index in kf:
-    		reg.fit(X[train_index],y[train_index])
-	preVals.append(reg.predict(X))
+    		reg.fit(X[train_index],y[train_index].ravel())
 
+# predict
+preStartTime = '2016-11-01'
+preEndTime = '2016-11-14'
+daysRange = pd.date_range(preStartTime,preEndTime)
+
+# origin data
+data1 = pd.read_csv('raw-data/1',index_col='time_stamp')
+sortedData = data1.sort_index()
+index = np.array([x for x in range(len(sortedData))])
+sortedData=sortedData.reset_index()
+sortedData['time_stamp'] = pd.to_datetime(sortedData['time_stamp'])
+
+for i in range(0,len(daysRange)):
+
+		# insert pre datetime to sortedData
+		insertDay = pd.DataFrame([[daysRange[i],0]],columns=['time_stamp','pay_sum'])
+		sortedData = pd.concat([sortedData,insertDay],ignore_index=True)
+		loc = list(sortedData['time_stamp']).index(pd.Timestamp(daysRange[i]))
+		
+		# compute feature vals
+		_dayofweek=daysRange[i].dayofweek
+		_m2M=m2M(sortedData,daysRange[i])
+		_w2A=w2A(sortedData,daysRange[i])
+		_m1L=m1L(sortedData,pd.Timestamp(daysRange[i]))
+		_d3w3=d3w3(sortedData,pd.Timestamp(daysRange[i]))
+		
+		# insert feature to data(feature data)
+		insertDay_2 = pd.DataFrame([[0,_dayofweek,_m2M,_w2A,_m1L,_d3w3]],columns=['pay_sum','dayofweek','m2M','w2A','m1L','d3w3'])
+		data = pd.concat([data,insertDay_2],ignore_index=True)
+        	
+		# predict vals
+		preVals = []
+		sampleIndex = len(data)-1
+		for reg in regressors:
+			sample = []
+			newSample = data.loc[sampleIndex]
+			sample.append(newSample['dayofweek'])
+			sample.append(newSample['m2M'])
+			sample.append(newSample['w2A'])
+			sample.append(newSample['m1L'])
+			sample.append(newSample['d3w3'])
+			#sample = np.array([data.loc[sampleIndex].tolist()])
+			saple = np.array([sample])
+			preVals.append(reg.predict(sample))	
+		weights = np.array(weights)
+		preVals = np.array(preVals)
+		weights.shape = (4,1)
+		weights = np.transpose(weights)
+		comVals = weights.dot(preVals)
+		
+		# insert pre vals to sortedData as pay_sum for the next datetime
+		sortedData=sortedData.drop(loc)
+		insertDay = pd.DataFrame([[comVals[0][0],daysRange[i]]],columns=['pay_sum','time_stamp'])
+		
+		sortedData = pd.concat([sortedData,insertDay],ignore_index=True)
+
+f1 = open('data','w')
+data.to_csv(f1,index=False)
+f1.close()
+f2 = open('sortedData','w')
+sortedData.to_csv(f2,index=False)
+f2.close()
+# compute error
+preVals = []
+_X = data.loc[:,['dayofweek','m2M','w2A','m1L','d3w3']].as_matrix()
+for reg in regressors:
+	preVals.append(reg.predict(_X))
 weights = np.array(weights)
 preVals = np.array(preVals)
-weights.shape = (len(weights),1)
+weights.shape = (4,1)
 weights = np.transpose(weights)
-
-# compute error
 comVals = weights.dot(preVals)
+data['predict']=comVals.tolist()[0]
+label = ['pay_sum','predict']
+labelColor = ['red','green']
+for i in range(0,len(label)):
+	plt.plot(data[label[i]],color=labelColor[i],label=label[i])
+plt.legend(loc='best')
+plt.title('1')
+plt.show()
+plt.close()
+#print "predict vals:",comVals
 # total error
-err = (np.absolute(comVals - y.T)/np.absolute(comVals + y.T)).sum()
-print err
+#err = (np.absolute(comVals - y.T)/np.absolute(comVals + y.T)).sum()
+#print "score:",err
 
